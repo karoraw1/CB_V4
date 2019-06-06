7e1f7eb55a8fd0ec34ca31a0f841cdf0f425a1c0
Keith Arora-Williams <login@Keiths-MacBook-Air.local>
2019-06-06 17:27:28 -0400

    created marcc batch script

--- otu_scripts/beta_nearest_taxon_distance.R
+++ otu_scripts/beta_nearest_taxon_distance.R
@@ -11,45 +11,36 @@ library(doSNOW)
 
 ## Test Data
 
-tsv.data <- read.delim("../otu_data/clustered_sequences/test_abundances.tsv", row.names=1)
-phydf <- read.delim("../otu_data/clustered_sequences/test_distmat.tsv", row.names=1)
-strata_df <- read.delim("../otu_data/clustered_sequences/test_strata.tsv", row.names=1)
-phydist = as.matrix(phydf)
-strat_vec <- unname(unlist(strata_df[,'CollectionAgency']))
-
+#strata_df <- read.delim("../otu_data/clustered_sequences/test_strata.tsv", row.names=1)
+#strat_vec <- unname(unlist(strata_df[,'CollectionAgency']))
+#strata_ <- as.integer(as.factor(strat_vec)[drop = TRUE])
 start_time <- Sys.time()
-MNTD_w <- comdistnt.par(tsv.data, phydist, abundance.weighted = TRUE, cores=24)
-end_time <- Sys.time()
+tsv.data <- read.delim("../otu_data/clustered_sequences/test_abundances.tsv", row.names=1)
+phydf <- read.delim("../otu_data/clustered_sequences/fixed_pplacer_distmat.tsv", row.names=1)
+phydist = as.matrix(phydf)
+MNTD_w <- comdistnt.par(tsv.data, phydist, abundance.weighted = TRUE, cores=1)
 write(MNTD_w, stdout())
 save(MNTD_w, file="../otu_data/clustered_sequences/test_mean_nearest_taxon_distance.RData")
-end_time - start_time
-
-start_time <- Sys.time()
-mntd_scores <- ses.comdistnt2(tsv.data, phydist, strata = strat_vec, abundance.weighted = TRUE, runs = 333, iterations=333, cores=24)
-end_time <- Sys.time()
-write(mntd_scores, stdout())
+mntd_scores <- ses.comdistnt2(tsv.data, phydist, method = "quasiswap", strata = NULL, abundance.weighted = TRUE, runs = 5, cores=1)
+write(mntd_scores$comdistnt.obs.z, stdout())
 save(mntd_scores, file="../otu_data/clustered_sequences/test_standard_effects_mntd.RData")
+end_time <- Sys.time()
 end_time - start_time
 
 ## Real Data
 
+#strata_df <- read.delim("../otu_data/clustered_sequences/strata.tsv", row.names=1)
+#strat_vec <- unname(unlist(strata_df[,'CollectionAgency']))
+
+start_time <- Sys.time()
 tsv.data <- read.delim("../otu_data/clustered_sequences/pplacer_abundances.tsv", row.names=1)
 phydf <- read.delim("../otu_data/clustered_sequences/fixed_pplacer_distmat.tsv", row.names=1)
-strata_df <- read.delim("../otu_data/clustered_sequences/strata.tsv", row.names=1)
 phydist = as.matrix(phydf)
-strat_vec <- unname(unlist(strata_df[,'CollectionAgency']))
-
-start_time <- Sys.time()
 MNTD_w <- comdistnt.par(tsv.data, phydist, abundance.weighted = TRUE, cores=24)
-end_time <- Sys.time()
 save(MNTD_w, file="../otu_data/clustered_sequences/mean_nearest_taxon_distance.RData")
-end_time - start_time
-
-start_time <- Sys.time()
-mntd_scores <- ses.comdistnt2(tsv.data, phydist, strata = strat_vec, abundance.weighted = TRUE, runs = 333, iterations=333, cores=24)
 end_time <- Sys.time()
-save(mntd_scores, file="../otu_data/clustered_sequences/standard_effects_mntd.RData")
 end_time - start_time
-
-
-
+mntd_scores <- ses.comdistnt2(tsv.data, phydist, method = "quasiswap", strata = NULL, abundance.weighted = TRUE, runs = 500, cores=24)
+save(mntd_scores, file="../otu_data/clustered_sequences/standard_effects_mntd.RData")
+end_time <- Sys.time()
+end_time - start_time

41d94364214fe21d8935fb2d7fd3306a183e9823
karoraw1 <karoraw1@jhu.edu>
2019-06-07 12:06:19 -0400

    we have bmntd files and effect sizes

--- otu_scripts/run_bntd.sh
+++ otu_scripts/run_bntd.sh
@@ -2,7 +2,7 @@
 
 #SBATCH
 #SBATCH --job-name=bmntd
-#SBATCH --time=72:00:00
+#SBATCH --time=8:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=100G
 #SBATCH --partition=parallel
@@ -12,6 +12,9 @@
 #SBATCH --mail-user=karoraw1@jhu.edu
 #SBATCH --error=bntd_par.err
 #SBATCH --output=bntd_par.out
+
+
+# takes 4-5 hours on 370 x 1900 otu table
 
 ml R/3.5.1
 Rscript beta_nearest_taxon_distance.R

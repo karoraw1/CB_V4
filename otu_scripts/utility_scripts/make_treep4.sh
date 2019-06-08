0c00a4cc0510c76adcaa697fbc1836671a6f8ac0
karoraw1 <karoraw1@jhu.edu>
2019-06-08 11:07:10 -0400

    new tree

--- otu_scripts/utility_scripts/make_treep4.sh
+++ otu_scripts/utility_scripts/make_treep4.sh
@@ -6,15 +6,14 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=100G
-#SBATCH --partition=parallel
-#SBATCH --exclusive
+#SBATCH --partition=lrgmem
 #SBATCH --mail-type=END 
 #SBATCH --mail-user=karoraw1@jhu.edu
 #SBATCH --error=mt4.err
 #SBATCH --output=mt4.out
 
 LOC_DIR=~/scratch/CB_V4/otu_data/tree_data 
-NEW_TD=$LOC_DIR/full_tree
+NEW_TD=$LOC_DIR/not_full_tree
 
 mkdir -p $NEW_TD
 
@@ -24,7 +23,7 @@ QUERY=query_high_abund
 
 source activate otu_caller
 
-tr "[ -%,;\(\):=\.\\\[]\"\']" "_" < ../$QUERY.fasta > $QUERY.fasta
+cp ../$QUERY.fasta .
 
 cmalign --cpu 24 --dna -o $QUERY.align.sto --sfile cm_scores.txt --outformat Pfam ../16S_bacteria.cm $QUERY.fasta
 
@@ -35,10 +34,10 @@ raxmlHPC-PTHREADS -T 24 -m GTRGAMMA -f I -t RAxML_bestTree.$QUERY.ref.tre -n roo
 raxmlHPC-PTHREADS -T 24 -m GTRGAMMA -f J -p 12345 -t RAxML_rootedTree.root.$QUERY.ref.tre -n conf.root.$QUERY.ref.tre -s $QUERY.align.fasta
 raxmlHPC-PTHREADS -T 24 -f x -p 12345 -t RAxML_rootedTree.root.$QUERY.ref.tre -s $QUERY.align.fasta -m GTRGAMMA -n $QUERY.distmat
 
-pplacer -o $QUERY.align.jplace -p -c ../hug_tol.refpkg $QUERY.align.fasta
-guppy to_csv --point-mass --pp -o $QUERY.align.csv $QUERY.align.jplace
-guppy fat --node-numbers --point-mass --pp -o $QUERY.align.phyloxml $QUERY.align.jplace
-guppy distmat -o $QUERY.align.dist.tab $QUERY.align.jplace
+#pplacer -o $QUERY.align.jplace -p -c ../hug_tol.refpkg $QUERY.align.fasta
+#guppy to_csv --point-mass --pp -o $QUERY.align.csv $QUERY.align.jplace
+#guppy fat --node-numbers --point-mass --pp -o $QUERY.align.phyloxml $QUERY.align.jplace
+#guppy distmat -o $QUERY.align.dist.tab $QUERY.align.jplace
 
 
 

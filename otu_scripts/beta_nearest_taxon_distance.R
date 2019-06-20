ace52e2d36d8f3e891a03a6132c4c7658e5987d7
Keith Arora-Williams <login@Keiths-MacBook-Air.local>
2019-06-20 12:14:22 -0400

    wrote out fresh distmat and added converter

--- otu_scripts/beta_nearest_taxon_distance.R
+++ otu_scripts/beta_nearest_taxon_distance.R
@@ -1,19 +1,12 @@
-
+library(vegan)
 library(ape)
 library(picante)
 library(MicEco)
 library(doSNOW)
 
-#tree_90 = read.tree(file = "../otu_data/clustered_sequences/cluster_members.100.phylip32.mod.nh")
-#tsv.data <- read.delim("../otu_data/clustered_sequences/abundances.100.tsv", row.names=1)
-#prunedphy <- prune.sample(tsv.data, tree_90)
-#phydist <- cophenetic(prunedphy)
+### Calculate NTR and NTI
 
 ## Test Data
-
-#strata_df <- read.delim("../otu_data/clustered_sequences/test_strata.tsv", row.names=1)
-#strat_vec <- unname(unlist(strata_df[,'CollectionAgency']))
-#strata_ <- as.integer(as.factor(strat_vec)[drop = TRUE])
 start_time <- Sys.time()
 tsv.data <- read.delim("../otu_data/clustered_sequences/test_abundances.tsv", row.names=1)
 phydf <- read.delim("../otu_data/clustered_sequences/fixed_pplacer_distmat.tsv", row.names=1)
@@ -28,10 +21,6 @@ end_time <- Sys.time()
 end_time - start_time
 
 ## Real Data
-
-#strata_df <- read.delim("../otu_data/clustered_sequences/strata.tsv", row.names=1)
-#strat_vec <- unname(unlist(strata_df[,'CollectionAgency']))
-
 start_time <- Sys.time()
 tsv.data <- read.delim("../otu_data/clustered_sequences/pplacer_abundances.tsv", row.names=1)
 phydf <- read.delim("../otu_data/clustered_sequences/fixed_pplacer_distmat.tsv", row.names=1)
@@ -44,3 +33,5 @@ mntd_scores <- ses.comdistnt2(tsv.data, phydist, method = "quasiswap", strata = 
 save(mntd_scores, file="../otu_data/clustered_sequences/standard_effects_mntd.RData")
 end_time <- Sys.time()
 end_time - start_time
+
+
